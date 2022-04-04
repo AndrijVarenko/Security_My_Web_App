2ea42878ed7eb94fa70912ad4ceb3cfb3f1942d9
AndrijVarenko <andrijvarenko@gmail.com>
2022-04-04 22:00:08 +0300

    Initial commit

---
+++ src/main/java/com/example/sec_my_app/SecMyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sec_my_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecMyAppApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SecMyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sec_my_app/SecMyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sec_my_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecMyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
